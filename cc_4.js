16bbbd2fd8320d79f5336c7b145d1742c2335cb2
Gabriela Serrano <gabrielasc0309@gmail.com>
2025-01-29 04:19:51 -0500

    Task 1 - Customer Discounts

--- cc_4.js
+++ cc_4.js
@@ -1,1 +1,11 @@
 console.log("JavaScript file is linked successfully!");
+// Task 1 - Customer Discounts
+let purchaseAmount = 120; // Change this value to test different amounts
+
+if (purchaseAmount > 100) {
+    let discount = purchaseAmount * 0.1; // 10% discount
+    let finalAmount = purchaseAmount - discount;
+    console.log(`Final amount after discount: $${finalAmount}`);
+} else {
+    console.log(`Final amount: $${purchaseAmount}`);
+}
